97462ac2ac2941abf16430576abcda453a903c7d
tlsrlwo <liongijae@naver.com>
2025-05-26 00:55:41 +0900

    Making Level Buttons

---
+++ Assets/1____________ProjectPlatformer________________/Scripts/TitleScene/LevelMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelMenu : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject levelButtons;
+
+    private void Awake()
+    {
+        ButtonsArray();
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void ButtonsArray()
+    {
+        int childCount = levelButtons.transform.childCount;
+        buttons = new Button[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            buttons[i] = levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
+    }
+}
